17640fd5735d4d7f7beadc2eb772bc48f35af60c
Jin0331 <sempre813@naver.com>
2024-03-07 14:09:42 +0900

    Initial Commit

---
+++ Pill-IT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pill-IT
+//
+//  Created by JinwooLee on 3/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
